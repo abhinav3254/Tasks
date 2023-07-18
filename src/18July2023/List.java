a972e6c66a3fb4de5d05130271650b3eb5b81203
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-07-18 22:49:17 +0530

    List Task Done

---
+++ src/18July2023/List.java
@@ -0,0 +1,112 @@
+package assignment;
+
+public class List {
+	Node head = null;
+	Node tail = null;
+	int size = -1;
+	public void addElement(int data) {
+		Node node = new Node(data);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			while (tail.next != null) {
+				tail = tail.next;
+			}
+			tail.next = node;
+			tail = tail.next;
+		}
+		size = size+1;
+	}
+	
+	public void addAtStart(int data) {
+		Node temp = null;
+		Node node = new Node(data);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			temp = head;
+			head = node;
+			head.next = temp;
+		}
+		size = size + 1;
+	}
+	
+	public void addAtLast(int data) {
+		Node node = new Node(data);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			while (tail.next!=null) {
+				tail = tail.next;
+			}
+			tail.next = node;
+			tail = tail.next;
+		}
+		size = size + 1;
+	}
+	
+	public void printAll () {
+		Node temp = head;
+		while(temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+	
+	public void deleteAtStart() {
+		head = head.next;
+		size = size-1;
+	}
+	
+	public void deleteAtEnd() {
+		Node temp = head;
+		while (temp.next.next!=null) {
+			temp = temp.next;
+		}
+		tail = temp.next;
+		tail.next = null;
+		temp.next = null;
+		size = size - 1;
+	}
+	
+	public void deleteAt(int index) {
+		if (size < 0) {
+			System.out.println("Empty List");
+			return;
+		} else if (index >= size) {
+			System.out.println("Wrong index");
+			return;
+		} else if (index == size - 1) {
+			deleteAtEnd();
+			return;
+		} else if (index == 0) {
+			deleteAtStart();
+			return;
+		}else{
+			Node temp = head;
+			Node temp2 = head.next;
+			int i = 0;
+			while (++i < index) {
+				temp = temp2;
+				temp2 = temp2.next;
+			}
+			temp.next = temp2.next;
+			size = size - 1;
+			return;
+		}
+	}
+}
+
+class Node {
+	int data;
+	Node next;
+	
+	public Node (int data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+}
---
+++ src/18July2023/Main.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class Main {
+	public static void main(String[] args) {
+		List list = new List();
+		list.addElement(10);
+		list.addElement(20);
+		list.addElement(30);
+		list.addElement(40);
+		list.addAtStart(101);
+		list.addAtLast(202);
+		list.printAll();
+		System.out.println("__________");
+		//list.deleteAtStart();
+		//list.printAll();
+		//list.deleteAtEnd();
+		list.deleteAt(2);
+		System.out.println("__________");
+		list.printAll();
+	}
+}
