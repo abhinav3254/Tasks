09db7cfa8b9045397c23dd8d83e9f4c3fe0f2ac4
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-07-19 10:12:14 +0530

    genrics list

--- src/18July2023/Main.java
+++ src/18July2023/Main.java
@@ -1,21 +1,28 @@
-package assignment;
+package july19;
+
 
 public class Main {
 	public static void main(String[] args) {
-		List list = new List();
-//		list.addElement(10);
-//		list.addElement(20);
-//		list.addElement(30);
-//		list.addElement(40);
-//		list.addAtStart(101);
-//		list.addAtLast(202);
+		
+		// Note : indexing start from 0
+		List<String> list = new List<String>();
+		list.addElement("10");
+		list.addElement("20");
+		list.addElement("30");
+		list.addElement("40");
+		list.addAtStart("101");
+		list.addAtLast("202");
 		list.printAll();
 		System.out.println("__________");
-		//list.deleteAtStart();
-		//list.printAll();
-		//list.deleteAtEnd();
+		list.deleteAtStart();
+		list.printAll();
+		list.deleteAtEnd();
 		list.deleteAt(2);
 		System.out.println("__________");
+		list.printAll();
+		System.out.println("__________");
+		list.deleteAt(2);
+		System.out.println("Delete at inde x2");
 		list.printAll();
 	}
 }
